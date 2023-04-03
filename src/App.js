90cf5c10cc81147bb4d91e02cccf852d1783602d
SHAURYA-KUMAR22 <101716257+SHAURYA-KUMAR22@users.noreply.github.com>
2023-04-03 18:33:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,105 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import SingleCard from "./components/SingleCard";
+
+const cardImages = [
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
+];
+
+function App() {
+  //state to store shuffled card array and state for turns
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+  //shuffle cards
+  const shuffleCards = () => {
+    //12 size array with each card twice
+    const shuffleCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    //in case user already selected one card and then hit new game
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setCards(shuffleCards);
+    //on clicking new game turns should again go to 0
+    setTurns(0);
+  };
+
+  //handle a choice
+  const handleChoice = (card) => {
+    //console.log(card)
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  //compare two cards
+  //fires once automatically and   when dependencies changes
+  useEffect(() => { 
+
+    if (choiceOne && choiceTwo) {
+      //disabled other cards till it compares the two choices
+      setDisabled(true)
+
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              //this will return new object instead of modifying state directly
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000)
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  console.log(cards, turns);
+
+  //reset choices and increase turn
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+    setDisabled(false);
+  };
+
+  //start game automatically
+  useEffect(() => {
+    shuffleCards()
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard 
+          key={card.id} 
+          card={card} 
+          handleChoice={handleChoice} 
+          flipped={card === choiceOne || card === choiceTwo || card.matched}
+          disabled={disabled}
+          />
+        ))}
+      </div>
+      <p>Turns: {turns}</p>
+    </div>
+  );
+}
+
+export default App;
